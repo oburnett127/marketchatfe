c8950e86e271683588a551149de7a6ebb34da240
Matthew McGrath <rubywep@hotmail.com>
2022-03-12 18:28:48 -0800

    Add NavButton components to Home page
    These serve as a quick test to see if our custom Tailwind-based components are working, as well as a way for us to quickly navigate to the different Pages we are working on.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../../components/NavButton/NavButton';
 
 function Home() {
   return (
@@ -8,6 +9,14 @@ function Home() {
         Here we will see a landing section of some sort, as well as a location
         for the user to sign in
       </p>
+      <div className='flex flex-wrap gap-2'>
+        <Button to='/'>Go Home</Button>
+        <Button to='/about'>About</Button>
+        <Button to='/profile'>Profile</Button>
+        <Button to='/signin'>Sign In</Button>
+        <Button to='/signup'>Sign Up</Button>
+        <Button to='/posts'>Posts</Button>
+      </div>
     </div>
   );
 }

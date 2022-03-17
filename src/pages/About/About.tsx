fa77f19253c3a99c0fefa18410d47ff6a15ca574
Matthew McGrath <rubywep@hotmail.com>
2022-03-16 17:50:36 -0700

    Minor styling to unify page with others.

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function About() {
   return (
-    <div>
-      <h1>This is the About page</h1>
+    <div className='min-h-screen px-44 text-center'>
+      <h1 className='text-3xl mb-10'>About Page</h1>
       <p>Here we will present information about...The site? The developers?</p>
     </div>
   );

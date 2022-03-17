585e4c7b1f77f67bcc5dfc49ca87f7f2ed6664ab
Matthew McGrath <rubywep@hotmail.com>
2022-03-16 17:51:08 -0700

    Add route for Post at '/posts/:postId'

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { About, Home, Posts, Profile, SignIn, SignUp } from './pages/';
-import Navigation from './components/Navigation/Navigation';
+import { About, Home, Post, Posts, Profile, SignIn, SignUp } from './pages/';
+import Navigation from './components/Navigation/Navigation.jsx';
 import Footer from './components/Footer/Footer';
+import { UserProvider } from './redux/UserContext';
 
 function App() {
   return (
     <Router>
-      <Navigation />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/posts' element={<Posts />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-      <Footer />
+      <UserProvider>
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/posts' element={<Posts />} />
+          <Route path='/posts/:postId' element={<Post />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+        <Footer />
+      </UserProvider>
     </Router>
   );
 }

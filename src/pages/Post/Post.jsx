b5a18fb59c932d5b2d3d0c4541ad7cccd1d5624b
Matthew McGrath <rubywep@hotmail.com>
2022-03-16 17:44:27 -0700

    Create Post page, which details a single Post
    On this page the user will be presented with all the comments made on a specific post. Very minimal styling down so far, and the Comments being fetched are from jsonplaceholder -- not a real database with posts / comments.

---
+++ src/pages/Post/Post.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+function Post() {
+  const { postId } = useParams();
+  const [post, setPost] = useState();
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    //   Fetch the comments for this post
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+      .then((response) => response.json())
+      .then((data) => {
+        setComments(data);
+        console.log(data);
+      });
+
+    // Fetch the post itself
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPost(data);
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <div className='min-h-screen px-44 '>
+      <h1 className='text-3xl mb-10 text-center'>
+        Comments for Post with ID {postId}
+      </h1>
+      <div className='mb-10'>
+        <h2 className='text-3xl font-bold'>{post && post.title}</h2>
+        <p>{post && post.body}</p>
+      </div>
+      <div className='flex flex-col gap-6 text-left'>
+        {comments.map((post) => {
+          return (
+            <div className='border-l-2 '>
+              <h2 className='text-sm font-bold pl-4'>{post.email}</h2>
+              <p className='pl-4'>{post.body}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Post;

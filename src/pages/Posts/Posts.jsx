3dad77785b0038b921b5664672171b4f7faabffd
Matthew McGrath <rubywep@hotmail.com>
2022-03-19 21:22:35 -0700

    Add Post fetching from db, improve styling
    Gave each post a background color that closer matches that of Reddit. In general, altered text colors, weights, and spacing to be more aligned with Reddit's. Various elements in a Post also direct the user to an appropriate page when clicked.
    
    More importantly, the first Post listed on the page is being fetched from a temporary backend!

--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -3,11 +3,16 @@ import { Link } from 'react-router-dom';
 
 function Posts() {
   const [posts, setPosts] = useState([]);
+  const [databasePosts, setDatabasePosts] = useState([]);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((response) => response.json())
       .then((data) => setPosts(data));
+
+    fetch('http://localhost:5000/api/comments')
+      .then((response) => response.json())
+      .then((data) => setDatabasePosts(data));
   }, []);
 
   return (
@@ -17,17 +22,69 @@ function Posts() {
         Here we will see a list of Posts created by the community. There will
         also probably be a way to sort / filter posts by various criteria.
       </p>
-      <div className='flex flex-col gap-6 text-left'>
+      <div className='flex flex-col gap-6 text-left mb-6'>
+        {databasePosts.map((post) => {
+          return (
+            <Link
+              key={post.id}
+              to={`/posts/${post._id}`}
+              className='bg-blue-900 p-4 rounded-lg border-solid border border-white hover:border-blue-500'
+            >
+              <div>
+                <Link to={`/r/stocks`} className='font-bold'>
+                  r/stocks
+                </Link>{' '}
+                <span className='text-gray-400 '>
+                  Posted by{' '}
+                  <Link
+                    to={`/users/${post.author}`}
+                    className='hover:underline'
+                  >
+                    {post.author}
+                  </Link>{' '}
+                </span>
+                <span className='text-gray-400'>5 days ago</span>
+              </div>
+              <h2 className='text-xl font-bold'>{post.title}</h2>
+              <p className='py-2'>{post.body}</p>
+              <Link
+                to={`/posts/${post.id}`}
+                className='p-1 rounded hover:bg-blue-600'
+              >
+                {Math.floor(Math.random() * 150)} comments
+              </Link>
+            </Link>
+          );
+        })}
+      </div>
+      <div className='flex flex-col gap-6 text-left '>
         {posts.map((post) => {
           return (
             <Link
               key={post.id}
               to={`/posts/${post.id}`}
-              className='p-4 border-solid border border-white hover:border-blue-500'
+              className='bg-zinc-700 p-4 rounded-lg border-solid border border-white hover:border-blue-500'
             >
+              <div>
+                <Link to={`/r/stocks`} className='font-bold'>
+                  r/stocks
+                </Link>{' '}
+                <span className='text-gray-400 '>
+                  Posted by{' '}
+                  <Link to={`/users/someDude`} className='hover:underline'>
+                    u/someDude
+                  </Link>
+                </span>
+                <span className='text-gray-400'>5 days ago</span>
+              </div>
               <h2 className='text-xl font-bold'>{post.title}</h2>
-              <p>{post.body}</p>
-              <p>{Math.floor(Math.random() * 150)} comments</p>
+              <p className='py-2'>{post.body}</p>
+              <Link
+                to={`/posts/${post.id}`}
+                className='p-1 rounded hover:bg-zinc-500'
+              >
+                {Math.floor(Math.random() * 150)} comments
+              </Link>
             </Link>
           );
         })}

efcb2bbd4363fefab2543ab4700d10c1553c65d3
Matthew McGrath <rubywep@hotmail.com>
2022-05-18 23:28:09 -0700

    Begin simple presentational About page component
    For now, this just has a header and uses the newly-created MemberCard to display information about the 'team'

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import MemberCard from './MemberCard';
 
 function About() {
   return (
-    <div className='min-h-screen px-44 text-center'>
-      <h1 className='text-3xl mb-10'>About Page</h1>
-      <p>Here we will present information about...The site? The developers?</p>
+    <div className='min-h-screen px-4 text-center bg-slate-200 dark:bg-stone-900 text-neutral-900 dark:text-white'>
+      <div className='m-auto max-w-screen-lg py-10'>
+        <h1 className='text-3xl mb-10'>About Page</h1>
+        <div className='p-8 bg-white dark:bg-stone-800 rounded-lg shadow'>
+          <p className='text-center text-3xl font-bold text-gray-800 dark:text-white'>Professional team</p>
+          <p className='text-center mb-12 text-xl font-normal text-gray-500 dark:text-gray-200'>
+            Meet the MarketChat Team!
+          </p>
+          <div className='flex items-center flex-col md:flex-row justify-evenly'>
+            <MemberCard
+              name='Matthew McGrath'
+              role='Lead Developer'
+              about='This is about Matthew'
+              image='https://source.unsplash.com/random/900×700/?person'
+            />
+            <MemberCard
+              name='Caitlin Gonzalez'
+              role='Stuff Do-er'
+              about='She did stuff, sure!'
+              image='https://source.unsplash.com/random/900×800/?person'
+            />
+            <MemberCard
+              name='Daniel McGrath'
+              role='Stuff Do-er'
+              about='He watched stuff being did!'
+              image='https://source.unsplash.com/random/900×750/?person'
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
